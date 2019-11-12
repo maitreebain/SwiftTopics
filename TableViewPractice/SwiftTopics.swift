6af3d7e6f00fb1669a181b885696345a40169da4
maitreebain <maitreebain@gmail.com>
2019-11-12 11:08:59 -0500

    Initial Commit

---
+++ TableViewPractice/SwiftTopics.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftTopics.swift
+//  TableViewPractice
+//
+//  Created by Maitree Bain on 11/12/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import Foundation
+
+let swiftTopics = ["Basic Operators", "Strings and Characters", "Collection Types", "Control Flow", "Functions",
+                   "Closures", "Enumerations", "Structures and Classes", "Properties", "Methods", "Subscripts",
+                   "Inheritance", "Initialization", "Deinitialization", "Optional Chaining", "Error Handling",
+                   "Type Casting", "Nested Types", "Extensions", "Protocols", "Generics", "Opaque Types",
+                   "Automatic Reference Counting", "Memory Safety", "Access Control", "Advanced Operators"
+]
---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Maitree Bain on 11/12/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swiftTopicsTableView: UITableView!
+    
+    let swiftTopics = ["Basic Operators", "Strings and Characters", "Collection Types", "Control Flow", "Functions",
+                       "Closures", "Enumerations", "Structures and Classes", "Properties", "Methods", "Subscripts",
+                       "Inheritance", "Initialization", "Deinitialization", "Optional Chaining", "Error Handling",
+                       "Type Casting", "Nested Types", "Extensions", "Protocols", "Generics", "Opaque Types",
+                       "Automatic Reference Counting", "Memory Safety", "Access Control", "Advanced Operators"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        swiftTopicsTableView.dataSource = self
+        
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftTopics.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "topicCell", for: indexPath)
+        
+        let swiftItem = swiftTopics[indexPath.row]
+        
+        cell.textLabel?.text = swiftItem
+        
+        return cell
+    }
+}
